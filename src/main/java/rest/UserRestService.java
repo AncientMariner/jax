5e446e4f16da3cc1d44aacb3eb1605d2ad148139
AncientMariner <xander.lysak@gmail.com>
2013-12-11 19:59:54 +0200

    uri matching

---
+++ src/main/java/rest/UserRestService.java
@@ -0,0 +1,30 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+public class UserRestService {
+
+    @GET
+    public Response getUser() {
+        return Response.status(200).entity("getUser is called").build();
+    }
+
+    @GET
+    @Path("/vip")
+    public Response getUserVIP() {
+        return Response.status(200).entity("getUserVIP is called").build();
+    }
+
+    @GET
+    @Path("{name}")
+    public Response getUserByName(@PathParam("name") String name) {
+
+        return Response.status(200)
+                .entity("getUserByName is called, name : " + name).build();
+
+    }
+}
