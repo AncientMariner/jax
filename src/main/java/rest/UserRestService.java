9681ad80875ec191e0c7b85f8741ccda9f65e97f
AncientMariner <xander.lysak@gmail.com>
2013-12-11 20:07:41 +0200

    path URI matching example

--- src/main/java/rest/UserRestService.java
+++ src/main/java/rest/UserRestService.java
@@ -27,4 +27,24 @@ public class UserRestService {
                 .entity("getUserByName is called, name : " + name).build();
 
     }
+
+    @GET
+    @Path("{id : \\d+}") //support digit only
+    public Response getUserById(@PathParam("id") String id) {
+        return Response.status(200).entity("getUserById is called, id : " + id).build();
+    }
+
+    @GET
+    @Path("/username/{username : [a-zA-Z][a-zA-Z_0-9]}")
+    public Response getUserByUserName(@PathParam("username") String username) {
+        return Response.status(200)
+                .entity("getUserByUserName is called, username : " + username).build();
+    }
+
+    @GET
+    @Path("/books/{isbn : \\d+}")
+    public Response getUserBookByISBN(@PathParam("isbn") String isbn) {
+        return Response.status(200)
+                .entity("getUserBookByISBN is called, isbn : " + isbn).build();
+    }
 }
